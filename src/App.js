a162d8e68520f0d8bce93e34ed5022e89db2f1d4
Alexandra Severyukhina <312908@mail.ru>
2020-04-29 01:06:14 +0400

    get data with proxy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo } from 'react';
+import Table from './components/Table';
+
+//const url = 'https://hq.asodesk.com/api/us/demo/keyword-analytics/data-stats';
+const url = 'http://localhost:3005/keyword-analytics/data-stats';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Keyword',
+        accessor: 'keyword',
+      },
+      {
+        Header: 'Traffic Score',
+        accessor: 'users_per_day',
+      },
+      {
+        Header: 'Total Apps',
+        accessor: 'total_apps',
+      },
+/*       {
+        Header: 'Rank',
+        accessor: 'position_info',
+      }, */
+    ],
+    [],
+  );
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch(url);
+      const result = await data.json();
+
+      console.log(result.data);
+
+      setData(result.data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table data={data} columns={columns} />
     </div>
   );
 }
